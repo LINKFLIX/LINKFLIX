14b796386609bfee9766a4a966821da71ac8694c
chaeha <chaeha93@gmail.com>
2021-10-20 15:46:08 +0900

    feat: Create spring boot project and Entity

---
+++ backend/src/main/java/com/linkflix/LinkflixApplication.java
@@ -0,0 +1,13 @@
+package com.linkflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkflixApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/java/com/linkflix/entity/Timeline.java
@@ -0,0 +1,25 @@
+package com.linkflix.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.sql.Time;
+
+@Entity(name="timelines")
+@Getter
+@Setter
+public class Timeline extends BaseEntity {
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name="productId", nullable = false)
+    Product product;
+
+    private String netflixEpisodeId;
+    private Time startTime;
+    private Time endTime;
+
+}
