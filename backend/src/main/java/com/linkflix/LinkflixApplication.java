42c83483a9250b2d9e36da13be122f6233baf559
chaeha <chaeha93@gmail.com>
2021-10-21 14:54:08 +0900

    test: CI/CD

--- backend/src/main/java/com/linkflix/LinkflixApplication.java
+++ backend/src/main/java/com/linkflix/LinkflixApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LinkflixApplication {
+public class LinkflixApplication { //
 
     public static void main(String[] args) {
         SpringApplication.run(LinkflixApplication.class, args);
