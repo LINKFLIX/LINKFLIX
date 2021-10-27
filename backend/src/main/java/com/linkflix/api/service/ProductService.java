e3bed5d3571ab8b3b2eaec30e9c6652869989f48
chaeha <chaeha93@gmail.com>
2021-10-27 15:45:04 +0900

    feat: Product CRUD

--- backend/src/main/java/com/linkflix/api/service/ProductService.java
+++ backend/src/main/java/com/linkflix/api/service/ProductService.java
@@ -1,11 +1,19 @@
 package com.linkflix.api.service;
 
 import com.linkflix.api.request.ProductReq;
+import com.linkflix.api.request.ProductUpdateReq;
 import com.linkflix.db.entity.Product;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Optional;
+
 public interface ProductService {
 
-    String saveProduct(MultipartFile file, ProductReq productReq) throws Exception;
+    Product saveProduct(MultipartFile file, ProductReq productReq) throws Exception;
+    Optional<Product> getProduct(Long productId);
+    Product updateProduct(MultipartFile file, ProductUpdateReq productReq) throws Exception;
+    void deleteProduct(Long productId);
 
+    String createProductImage(MultipartFile file) throws Exception;
+    void deleteProductImage(String imagePath);
 }
