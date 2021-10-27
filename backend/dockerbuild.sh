b7960a56f3349987907e8543373e6121dc7df8d5
chaeha <chaeha93@gmail.com>
2021-10-27 10:37:33 +0900

    fix: dockerbuild.sh

--- backend/dockerbuild.sh
+++ backend/dockerbuild.sh
@@ -1,4 +1,4 @@
 docker build -t linkflix ./
 docker ps -f name=linkflix  -q | xargs --no-run-if-empty docker container stop
 docker container ls -a -f name=linkflix -q | xargs -r docker container rm
-docker run -d -p 8080:8080 -v /home/ubuntu/upload-images:/upload-images --name linkflix linkflix
+docker run -d -p 8080:8080 -v /home/ubuntu/product_images:/product_images --name linkflix linkflix
