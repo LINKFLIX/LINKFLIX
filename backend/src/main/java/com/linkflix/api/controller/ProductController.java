33199a87c2b07c019cc38122ada8b949e221970e
chaeha <chaeha93@gmail.com>
2021-10-28 15:06:44 +0900

    fix: gradle build

--- backend/src/main/java/com/linkflix/api/controller/ProductController.java
+++ backend/src/main/java/com/linkflix/api/controller/ProductController.java
@@ -14,12 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.activation.MimetypesFileTypeMap;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Api(value = "상품(Product) API", tags = {"Product"})
 @CrossOrigin
