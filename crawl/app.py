8b0bb41d803dd39973c6ddc40c929dd75c11d0be
multicampus <kkokko-hero@hanmail.net>
2021-10-29 10:47:51 +0900

    Add get method

--- crawl/app.py
+++ crawl/app.py
@@ -1,17 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
-
 @app.route('/')
-def crawl():
-    item_name = '신민아 원피스'
+def crawl(name):
+    item_name = name
     # 쇼핑 주소
     r = requests.get(f'https://search.shopping.naver.com/search/all?frm=NVSHMDL&origQuery={item_name}&pagingIndex=1&pagingSize=40&productSet=model&query={item_name}&sort=rel&timestamp=&viewType=list')
     # soup parsing
@@ -21,9 +16,17 @@ def crawl():
     goods_soup = BeautifulSoup(requests.get(goods_url).content, 'html.parser')
     image_path = goods_soup.select('#__next > div > div.style_container__3iYev > div.style_inner__1Eo2z > div.style_content_wrap__2VTVx > div.style_content__36DCX > div > div.image_thumb_area__1dzNx > div.image_photo_area__44Fqz > div > img')[0]['src']
     data = {'id' : item_name, 'image_path' : image_path}
-    print(data)
     return data
 
+
+@app.route('/get', methods=['GET'])
+def method():
+    if request.method == 'GET':
+        args_dict = request.args.to_dict()
+        return crawl(args_dict['name'])
+
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
