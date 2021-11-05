5928add8827517102adfbb08a2fcda152edcd453
minho-jang <minho9301@naver.com>
2021-11-05 15:25:48 +0900

    fix: 카트 아이콘 크기 조정 및 애니메이션 적용

--- frontend/src/components/Sidebar/Sidebar.vue
+++ frontend/src/components/Sidebar/Sidebar.vue
@@ -1,24 +1,27 @@
 <template>
   <div>
     <button
-      class="btn-logo"
+      class="btn-cart p-0"
       type="button"
       data-bs-toggle="offcanvas"
       data-bs-target="#offcanvasRight"
       aria-controls="offcanvasRight"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="48"
-        height="48"
-        fill="currentColor"
-        class="bi bi-cart3"
-        viewBox="0 0 16 16"
+      <div
+        class="d-flex justify-content-center align-items-center"
+        style="width: 4rem; height: 4rem"
       >
-        <path
-          d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-        />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          class="bi bi-cart3"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+          />
+        </svg>
+      </div>
     </button>
 
     <div
@@ -51,12 +54,15 @@ import ProductList from '../ProductList/ProductList.vue';
 import { logoEncodedBase64 } from '../../assets/images/logo';
 </script>
 
-<style>
-.btn-logo {
+<style scoped>
+.btn-cart {
   background-color: transparent;
   border: 0;
   outline: 0;
   color: white;
-  font-size: 3em;
+  transition: transform 150ms ease 0s;
+}
+.btn-cart:hover {
+  transform: scale(1.3);
 }
 </style>
