d1f39d954796561ae0db715e5d7f6b2daf45e62a
chungwy0110 <chungwy0110@naver.com>
2021-11-17 15:05:10 +0900

    fix: slide design

--- landingpage/src/components/SlideSecond.vue
+++ landingpage/src/components/SlideSecond.vue
@@ -1,9 +1,104 @@
 <template>
-  <div>SecondSlidePage</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-4" style="text-align: left">
+        <div style="font-weight: 900; font-size: 90px; margin-bottom: 100px">
+          <p>MENUAL</p>
+        </div>
+        <div style="font-size: 30px; font-weight: 600">
+          <p style="margin-bottom: 35px">버튼만 누르면</p>
+          <p style="margin-bottom: 35px">영상 속 제품을 쉽고 편하게</p>
+          <p style="margin-bottom: 36px">구매하실 수 있습니다</p>
+        </div>
+      </div>
+      <div class="col-8">
+        <swiper
+          :spaceBetween="30"
+          :centeredSlides="true"
+          :navigation="false"
+          :loop="true"
+          :autoplay="{
+            delay: 2500,
+          }"
+        >
+          <swiper-slide>
+            <img src="../img/ep1_0.png" />
+          </swiper-slide>
+          <swiper-slide>
+            <img src="../img/ep1_1.png" />
+          </swiper-slide>
+          <swiper-slide>
+            <img src="../img/ep1_2.png" />
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+import SwiperCore, { Autoplay, Pagination } from 'swiper/core';
+
+SwiperCore.use([Autoplay, Pagination]);
+export default defineComponent({
   name: 'slideSecond',
-};
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  methods: {
+    onSwiper(swiper: any) {
+      console.log(swiper);
+    },
+    onSlideChange() {
+      console.log('slide change');
+    },
+  },
+});
 </script>
+
+<style scoped>
+.title {
+  list-style: none;
+  padding-left: 0px;
+  font-size: 20px;
+  font-weight: 700;
+}
+.eplist {
+  list-style: none;
+  padding-left: 0px;
+  font-size: 15px;
+  font-weight: 500;
+}
+li {
+  float: left;
+}
+.panel {
+  list-style: none;
+}
+.swiper-container {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
--- landingpage/src/components/SlideThird.vue
+++ landingpage/src/components/SlideThird.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>ThirdSlidePage</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-4" style="text-align: left">
+        <div style="font-weight: 900; font-size: 90px; margin-bottom: 100px">
+          <p>BETA</p>
+        </div>
+        <div style="font-size: 30px; font-weight: 600">
+          <p style="margin-bottom: 35px">현재 LINKFLIX는</p>
+          <p style="margin-bottom: 35px">일부 영상에서 이용 가능합니다</p>
+          <p style="margin-bottom: 36px">서비스 영상은</p>
+          <p style="margin-bottom: 36px">지속적으로 업데이트 됩니다</p>
+        </div>
+      </div>
+      <div class="col-8" style="display: flex; justify-content: space-around">
+        <ul class="title">
+          <li>
+            <img
+              :src="`https://file.mk.co.kr/meet/neds/2021/08/image_readtop_2021_774018_16285690364746521.jpg`"
+              style="width: 330px; height: 470px; margin-bottom: 25px"
+            />
+            <p>갯마을 차차차 1 ~ 3 화</p>
+          </li>
+        </ul>
+        <ul class="title">
+          <li>
+            <img
+              :src="`https://upload.wikimedia.org/wikipedia/ko/6/60/%EA%B8%B0%EC%83%9D%EC%B6%A9_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg`"
+              style="
+                width: 330px;
+                height: 470px;
+                margin-bottom: 25px;
+                border: solid 1px lightgrey;
+              "
+            />
+            <p>기생충</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,3 +46,21 @@ export default {
   name: 'slideThird',
 };
 </script>
+
+<style scoped>
+.title {
+  list-style: none;
+  padding-left: 0px;
+  font-size: 20px;
+  font-weight: 700;
+}
+.eplist {
+  list-style: none;
+  padding-left: 0px;
+  font-size: 15px;
+  font-weight: 500;
+}
+li {
+  float: left;
+}
+</style>
