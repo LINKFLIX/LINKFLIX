2f044043ff84f1cc48eef9aa728d8f9545bf907d
신주환 <jason-shin94@hanmail.net>
2021-11-17 14:32:07 +0900

    fix : first slide

--- landingpage/src/components/SlideExample.vue
+++ landingpage/src/components/SlideExample.vue
@@ -9,6 +9,12 @@
       <img
         src="https://w.namu.la/s/5cd695b54231bb2af4d801ef59881da0bd343d71486ab2c81264059581ce4193df40d08c2f0240e79529c63196628a9c591d605563d4af4c963e6417892f81a2fd9bace704779477f6bc2dcf1da880ea856f2a39f27edc02975393a59a1d369a"
         alt="갯마을차차차"
+        style="width: 30%"
+      />
+      <img
+        src="https://w.namu.la/s/5cd695b54231bb2af4d801ef59881da0bd343d71486ab2c81264059581ce4193df40d08c2f0240e79529c63196628a9c591d605563d4af4c963e6417892f81a2fd9bace704779477f6bc2dcf1da880ea856f2a39f27edc02975393a59a1d369a"
+        alt="갯마을차차차"
+        style="width: 30%; margin-left: 10px"
       />
     </template>
   </SlideTemplate>
--- landingpage/src/components/SlideFirst.vue
+++ landingpage/src/components/SlideFirst.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="content">
-    <img
-      :src="imgsrc"
-      alt="linkflix"
-      style="width: 50%"
-      class="linkflix-logo"
-    />
     <h1>넷플릭스를 보다가 영상 속 제품에 대해서 궁금한 적 없으신가요?</h1>
     <h2>
-      <p style="color: blue; display: inline">LINK</p>
-      <p style="color: red; display: inline">FLIX</p>
+      <img
+        :src="imgsrc"
+        alt="linkflix"
+        style="width: 350px"
+        class="linkflix-logo"
+      />
       가 알려드립니다!
     </h2>
-    <button v-on:click="moveTo">설치하러가기</button>
+    <button type="button" class="btn btn-info" v-on:click="moveTo">
+      Download
+    </button>
   </div>
 </template>
 
@@ -37,8 +37,7 @@ export default {
 
 <style scoped>
 .linkflix-logo {
-  display: block;
-  margin-top: 10%;
+  display: inline;
   margin-left: auto;
   margin-right: auto;
 }
@@ -46,46 +45,25 @@ export default {
   position: relative;
   height: 100%;
   width: 100%;
-  background-color: #3c3e42;
+  margin-left: 5%;
   /* background: linear-gradient(-45deg, #94969c, #3c3e42, #000000); */
   background-size: 400% 400%;
   animation: gradient 15s ease infinite;
   vertical-align: middle;
 }
 
-@keyframes gradient {
-  0% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
-}
-
-@font-face {
-  font-family: 'SBAggroB';
-  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SBAggroB.woff')
-    format('woff');
-  font-weight: normal;
-  font-style: normal;
-}
 h1 {
-  font-family: 'SBAggroB';
-  font-size: 50px;
-  color: white;
+  margin-top: 10%;
+  font-size: 120px;
 }
 
 h2 {
-  font-family: 'SBAggroB';
-  font-size: 50px;
-  color: white;
+  font-size: 120px;
 }
 button {
-  font-family: 'SBAggroB';
-  font-size: 30px;
+  margin-top: 5%;
+  margin-left: 35%;
+  font-size: 80px;
   background-color: #19b5e0;
   color: white;
   cursor: pointer;
