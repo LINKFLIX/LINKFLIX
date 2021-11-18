4e827962f20d5824bdb35d50ef5569f5ddc39092
minho-jang <minho9301@naver.com>
2021-11-18 17:11:41 +0900

    feat: 4번째 슬라이드 내용 수정

--- landingpage/src/components/SlideFourth.vue
+++ landingpage/src/components/SlideFourth.vue
@@ -1,39 +1,75 @@
 <template>
-  <SlideTemplate>
-    <template v-slot:subtitle>Beta</template>
-    <template v-slot:description>
-      <div>현재 LINKFLIX는 일부 영상에 한해서 이용 가능합니다.</div>
-    </template>
-    <template v-slot:image-container>
-      <div class="h-100 d-flex justify-content-center align-items-center">
-        <div class="row justify-content-center gap-3 m-0">
-          <div class="col-5 p-0">
-            <img
-              class="w-100 mb-2"
-              :src="`https://file.mk.co.kr/meet/neds/2021/08/image_readtop_2021_774018_16285690364746521.jpg`"
-              style="object-fit: contain"
-            />
-            <div class="text-center fs-5">갯마을 차차차 1 ~ 3 화</div>
-          </div>
-          <div class="col-5 p-0">
-            <img
-              class="w-100 mb-2 ml-1"
-              :src="`https://upload.wikimedia.org/wikipedia/ko/6/60/%EA%B8%B0%EC%83%9D%EC%B6%A9_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg`"
-              style="border: solid 1px lightgrey; object-fit: contain"
-            />
-            <div class="text-center fs-5">기생충</div>
-          </div>
-        </div>
+  <div class="w-100" style="padding: 140px 120px; height: 100vh">
+    <div class="d-flex h-100 overflow-auto align-items-center">
+      <div class="col-4 text-center">
+        <div class="display-5">Available Contents</div>
       </div>
-    </template>
-  </SlideTemplate>
+      <div class="col-8 text-center h-100" id="slide-fourth-swiper">
+        <swiper class="h-100" :spaceBetween="30" :loop="true">
+          <!-- :autoplay="{ delay: 2500 }" -->
+          <swiper-slide class="h-100">
+            <div class="d-flex w-100 h-100">
+              <div class="col m-3">
+                <img
+                  class="h-100 shadow"
+                  src="https://file.mk.co.kr/meet/neds/2021/08/image_readtop_2021_774018_16285690364746521.jpg"
+                  style="object-fit: contain"
+                />
+              </div>
+              <div
+                class="
+                  col
+                  d-flex
+                  flex-column
+                  align-items-center
+                  justify-content-center
+                  fs-4
+                "
+              >
+                <div>갯마을 차차차</div>
+                <div>Ep.01</div>
+                <div>Ep.02</div>
+                <div>Ep.03</div>
+              </div>
+            </div>
+          </swiper-slide>
+          <swiper-slide class="h-100">
+            <div class="d-flex w-100 h-100">
+              <div class="col m-3">
+                <img
+                  class="h-100 shadow"
+                  src="http://t1.daumcdn.net/movie/cab3b02a7b274bd6838b80a5e481fedf1559021787090"
+                  style="object-fit: contain"
+                />
+              </div>
+              <div
+                class="
+                  col
+                  d-flex
+                  flex-column
+                  align-items-center
+                  justify-content-center
+                  fs-3
+                "
+              >
+                <div>기생충</div>
+              </div>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import SlideTemplate from './SlideTemplate.vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
 
 export default defineComponent({
-  components: { SlideTemplate },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
 });
 </script>
